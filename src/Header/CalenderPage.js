bdc659b31db4cd239a1417b6662e627a922663f0
Mohit Gawande <mohitgawande65@gmail.com>
2022-12-23 15:10:20 +0530

    improved rendering number of holiday for each day

--- src/Header/CalenderPage.js
+++ src/Header/CalenderPage.js
@@ -3,17 +3,24 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import Holiday from './Holiday'
 export default function CalenerPage({ onClickAddHoliday, Holidays, setHolidays, dateS, setDate }) {
+
+    
     return (
         <div className='app d-flex  justify-content-center align-items-center'>
             <div className='calendar-container my-3'>
                 <p>Please Select Any Date to add Holiday then click on Add Holiday Button</p>
                 <Calendar className='w-100' onChange={setDate} value={dateS} tileContent={
+
                     ({ date, view }) => {
-                        return (
-                            Holidays[dateS.toDateString()] && Holidays[dateS.toDateString()].map((item) => {
-                                return date.toDateString() == dateS.toDateString() && <Holiday />
-                            })
-                        )
+
+                            return Object.keys(Holidays).includes(date.toDateString()) && (
+                               Holidays[date.toDateString()].map((item,index) => {
+                                   console.log(item)
+                                    return  <Holiday /> 
+                                })
+                            )
+                        
+
                     }
 
                 }>
@@ -30,7 +37,7 @@ export default function CalenerPage({ onClickAddHoliday, Holidays, setHolidays, 
                 {
                     Object.entries(Holidays).map((item) => {
                         return item.map((value, index) => {
-                            return item.length > index + 1 ? <p>Day :{item[0]} <li>{'' + item[index + 1]  }</li></p> : ''
+                            return item.length > index + 1 ? <p>Day :{item[0]} <li>{'' + item[index + 1]}</li></p> : ''
                         })
                     })
                 }
