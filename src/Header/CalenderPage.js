cd23c0e3bc0e878cbba555a414224ba963356414
Mohit Gawande <mohitgawande65@gmail.com>
2022-12-23 12:18:56 +0530

    added the total holiday list on ui

--- src/Header/CalenderPage.js
+++ src/Header/CalenderPage.js
@@ -3,7 +3,6 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import Holiday from './Holiday'
 export default function CalenerPage({ onClickAddHoliday, Holidays, setHolidays, dateS, setDate }) {
-
     return (
         <div className='app d-flex  justify-content-center align-items-center'>
             <div className='calendar-container my-3'>
@@ -26,7 +25,17 @@ export default function CalenerPage({ onClickAddHoliday, Holidays, setHolidays, 
                         return <li><span>{item}</span></li>
                     }) : <h4 className='text-center text-warning'>No Holiday Added For This Date</h4>}
                 </p>
+
+                <h3>Total Holiday List</h3>
+                {
+                    Object.entries(Holidays).map((item) => {
+                        return item.map((value, index) => {
+                            return item.length > index + 1 ? <p>Day :{item[0]} <li>{'' + item[index + 1]  }</li></p> : ''
+                        })
+                    })
+                }
             </div>
+
         </div>
     );
 }
